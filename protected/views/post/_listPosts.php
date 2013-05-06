dfce4d3e5d7cf4fa2cb860d65c01a133f6ad28fe
ekip <ekip@gmx.de>
2013-05-06 10:42:08 +0200

    Datum und Uhrzeit wird bei den Posts angezeigt

--- protected/views/post/_listPosts.php
+++ protected/views/post/_listPosts.php
@@ -5,12 +5,14 @@ foreach ($posts as $post)
 	<article>
 		<header>
 			<h1><?php echo $post->title; ?></h1>
+			<h4><?php echo date('d.m.Y, H:i \U\h\r', $post->createTime); ?></h4>
 		</header>
 		<p><?php echo $post->content; ?></p>
 		<footer>
 			<p>von <?php echo $post->authorId; ?> in <?php echo $post->category; ?></p>
 		</footer>
 	</article>
+	<hr>
 <?php
 }
 ?>
